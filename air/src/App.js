8844059738f33d85804a89a310a1e8b2a82e4038
Tommy Wisaeu <kingdomkeygaming@gmail.com>
2019-03-09 20:18:39 -0600

    ?

--- air/src/App.js
+++ air/src/App.js
@@ -4,28 +4,28 @@ import './App.css';
 class App extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       info: '',
       aqius: '',
       cityInput: '',
       stateInput: '',
-      states: ['Alabama','Alaska','American Samoa','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','District of Columbia','Federated States of Micronesia','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Northern Mariana Islands','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Puerto Rico','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virgin Island','Virginia','Washington','West Virginia','Wisconsin','Wyoming'],
+      states: ['Alabama','Alaska','Arizona','Arkansas','California','Colorado','Connecticut','Delaware','Florida','Georgia','Guam','Hawaii','Idaho','Illinois','Indiana','Iowa','Kansas','Kentucky','Louisiana','Maine','Marshall Islands','Maryland','Massachusetts','Michigan','Minnesota','Mississippi','Missouri','Montana','Nebraska','Nevada','New Hampshire','New Jersey','New Mexico','New York','North Carolina','North Dakota','Ohio','Oklahoma','Oregon','Palau','Pennsylvania','Rhode Island','South Carolina','South Dakota','Tennessee','Texas','Utah','Vermont','Virginia','Washington','West Virginia','Wisconsin','Wyoming'],
       cities: [],
-    }
+    };
 
-    this.handleCityChange = this.handleCityChange.bind(this)
-    this.handleStateChange = this.handleStateChange.bind(this)
-    this.getData = this.getData.bind(this)
-    this.renderResults = this.renderResults.bind(this)
+    this.handleCityChange = this.handleCityChange.bind(this);
+    this.handleStateChange = this.handleStateChange.bind(this);
+    this.getData = this.getData.bind(this);
+    this.renderResults = this.renderResults.bind(this);
   }
 
   handleCityChange(event) {
     this.setState({
       cityInput: event.target.value,
-    })
-  }
+    });
+  };
 
   handleStateChange(event) {
     this.setState({
@@ -34,13 +34,13 @@ class App extends Component {
       if (!this.state.states.includes(this.state.stateInput)) { 
         this.setState({
           cities: [],
-        })
+        });
         return false; 
-      }
+      };
   
-      const res = await fetch(`https://api.airvisual.com/v2/cities?state=${this.state.stateInput}&country=USA&key=CPaiW6P6xGxFyrP3D`)
+      const res = await fetch(`https://api.airvisual.com/v2/cities?state=${this.state.stateInput}&country=USA&key=CPaiW6P6xGxFyrP3D`);
   
-      const data = await res.json()
+      const data = await res.json();
   
   
       if (data.status === 'success') {
@@ -49,16 +49,16 @@ class App extends Component {
         
         for (let i of data.data) {
           Arr.push(i.city)
-        }
+        };
   
         this.setState({
           cities: Arr
-        })
-      }
-    }.bind(this))
+        });
+      };
+    }.bind(this));
     
   
-  }
+  };
 
 
 
@@ -66,28 +66,32 @@ class App extends Component {
 
 
     if (!this.state.cityInput || !this.state.stateInput) {
+      this.setState({
+        aqius: ''
+      });
       return false;
     }
 
     else if (!this.state.states.includes(this.state.stateInput)) {
       this.setState({
-        info: 'State is not valid'
-      })
+        info: 'State is not valid',
+        aqius: ''
+      });
 
       return false;
-    }
+    };
 
-    this.setState({info: ''})
-    this.forceUpdate()
+    this.setState({info: ''});
+    this.forceUpdate();
 
-    const res = await fetch(`https://api.airvisual.com/v2/city?city=${this.state.cityInput}&state=${this.state.stateInput}&country=USA&key=CPaiW6P6xGxFyrP3D`)
+    const res = await fetch(`https://api.airvisual.com/v2/city?city=${this.state.cityInput}&state=${this.state.stateInput}&country=USA&key=CPaiW6P6xGxFyrP3D`);
 
-    const data = await res.json()
+    const data = await res.json();
 
     if (!data.data.current) {
       this.setState({
-        info: 'Sorry, either the info you inputted was incorrect or the city is not supported'
-      })
+        info: 'Either the info was incorrect or the city is not supported'
+      });
 
       return false;
     }
@@ -96,13 +100,32 @@ class App extends Component {
 
     this.setState({
       info: `The aqius of ${data.data.city} is ${aqius}`,
+      stateInput: '',
+      cityInput: '',
       aqius: aqius
-    })
-  }
+    });
+  };
 
   renderResults(data) {
+
+    let extraClass;
+
+    const aqius = this.state.aqius;
+
+    if (aqius !== '') {
+      if (aqius <= 50) { extraClass = 'green'; }
+      else if (aqius <= 100) { extraClass = 'yellow'; }
+      else if (aqius < 150) { extraClass = 'orange'; }
+      else { extraClass = 'red'; }
+    }
+
+ 
+
+
     if (data) {
-      return (<div className="results">{data}</div>)
+      return (
+        <div className="results"><span className={extraClass}>{data}</span></div>
+      )
     }
   }
 
@@ -116,7 +139,7 @@ class App extends Component {
 
           <div className="row">
 
-            <input list="states" onChange={this.handleStateChange} placeholder="State"/>
+            <input list="states" value={this.state.stateInput} onChange={this.handleStateChange} placeholder="State"/>
             <datalist id="states">
               {this.state.states.map((state, index) => {
                 return(
@@ -127,7 +150,7 @@ class App extends Component {
           </div>
 
           <div className="row">
-            <input list="cities" onChange={this.handleCityChange} placeholder="City"/>
+            <input list="cities" value={this.state.cityInput} onChange={this.handleCityChange} placeholder="City"/>
             <datalist id="cities">
             {this.state.cities.map((city, index) => {
                 return(
